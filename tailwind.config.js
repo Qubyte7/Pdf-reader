d11c3cff2f13208eb23c62db2bd07484f6cc51df
Qubyte7 <shamiinnocent123@gmail.com>
2025-08-01 18:48:52 +0000

    basic page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#BFCC3E',
+        'secondary': '#929767',
+        'background': '#1F1F1F',
+        'light-background': '#272727',
+        'danger': '#FF7300',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
